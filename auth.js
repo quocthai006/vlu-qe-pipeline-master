dceb6efe9029ff6376be9979d608d2edcaac177c
quocthai006 <quocthai8138@gmail.com>
2026-09-16 20:35:04 +0700

    Change admin password from '9999' to '123'

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
   // Tài khoản hợp lệ mặc định abc
-  if (username === 'admin' && password === '9999') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
 
